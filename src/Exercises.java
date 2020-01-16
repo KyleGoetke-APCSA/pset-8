ba04882bad2bc431fe6245ef27935d9504bbe500
Kyle Goetke <kgoetke@ucvts.tec.nj.us>
2020-01-16 13:56:39 -0500

    Fixed spacing

--- src/Exercises.java
+++ src/Exercises.java
@@ -8,102 +8,111 @@ public class Exercises {
     4) java -jar grader.jar [exercise name]
     */
 
-	public boolean commonEnd(int[] a, int[] b) {
-		if (a == null || a.length == 0 || b == null || b.length == 0) {
-			return false;
-		}
+    public boolean commonEnd(int[] a, int[] b) {
+        if (a == null || a.length == 0 || b == null || b.length == 0) {
+            return false;
+        }
 
-		// write your code here
         if ((a[0] == b[0]) || (a[a.length-1] == b[b.length-1])) {
             return true;
         }
 
-		return false;	// default return value to ensure compilation
-	}
+        return false;    // default return value to ensure compilation
+    }
 
-	public String[] endsMeet(String[] values, int n) {
+    public String[] endsMeet(String[] values, int n) {
         if (values == null || n < 0 && values.length < n) {
             return null;
         }
 
         // write your code here
-        String[] combinedString = new String[3];
-        combinedString[0] = values[0];
-        combinedString[1] = values[n];
+        String[] combinedString = new String[n * 2];
+
+
+        // go from first boi until nth boi
+        for (int i = 0; i < n; i++) {
+            combinedString[i] = values[i];
+        }
+
+        // go from length - nth boi until last boi
+        for (int i = combinedString.length - n; i < combinedString.length; i++) {
+            combinedString[i] = values[i];
+        }
+
         return combinedString;
 
-		// return null; default return value to ensure compilation
-	}
+        // return null; default return value to ensure compilation
+    }
 
-	public int difference(int[] numbers) {
+    public int difference(int[] numbers) {
         // input verification
 
         // write your code here
         // code
 
-		return -1;		// default return value to ensure compilation
-	}
+        return -1;        // default return value to ensure compilation
+    }
 
-	public double biggest(double[] numbers) {
+    public double biggest(double[] numbers) {
         // input verification
 
         // write your code here
         // code
 
-		return -1;		// default return value to ensure compilation
-	}
+        return -1;        // default return value to ensure compilation
+    }
 
-	public String[] middle(String[] values) {
+    public String[] middle(String[] values) {
         // input verification
 
         // write your code here
         // code
 
-		return null;	// default return value to ensure compilation
-	}
+        return null;    // default return value to ensure compilation
+    }
 
-	public boolean increasing(int[] numbers) {
+    public boolean increasing(int[] numbers) {
         // input verification
 
         // write your code here
         // code
 
-		return false;	// default return value to ensure compilation
-	}
+        return false;    // default return value to ensure compilation
+    }
 
-	public boolean everywhere(int[] numbers, int x) {
+    public boolean everywhere(int[] numbers, int x) {
         // input verification
 
         // write your code here
         // code
 
-		return false;	// default return value to ensure compilation
-	}
+        return false;    // default return value to ensure compilation
+    }
 
-	public boolean consecutive(int[] numbers) {
+    public boolean consecutive(int[] numbers) {
         // input verification
 
         // write your code here
         // code
 
-		return false;	// default return value to ensure compilation
-	}
+        return false;    // default return value to ensure compilation
+    }
 
-	public boolean balance(int[] numbers) {
+    public boolean balance(int[] numbers) {
         // input verification
 
         // write your code here
         // code
 
-		return false;	// default return value to ensure compilation
-	}
+        return false;    // default return value to ensure compilation
+    }
 
-	public int clumps(String[] values) {
+    public int clumps(String[] values) {
         // input verification
 
         // write your code here
         // code
 
-		return -1;		// default return value to ensure compilation
-	}
+        return -1;        // default return value to ensure compilation
+    }
 }
